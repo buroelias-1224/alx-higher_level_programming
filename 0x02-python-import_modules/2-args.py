4b7caa47b6318896de2b04edbbf340c21af73341
buroelias-1224 <104757613+buroelias-1224@users.noreply.github.com>
2022-08-26 05:14:26 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    i = len(sys.argv) - 1
+    from sys import argv
+    n = len(argv)
+    if n <= 1:
+        print("{:d} arguments.".format(n-1))
 
-    if i == 0:
-        print("{} arguments.".format(i))
-    elif i == 1:
-        print("{} argument:".format(i))
+    elif n == 2:
+        print("{:d} argument:".format(n-1))
+        for i in range(1, n):
+            print("{:d}: {}".format(i, argv[i]))
     else:
-        print("{} arguments:".format(i))
-
-    if i >= 1:
-        i = 0
-        for arg in sys.argv:
-            if i != 0:
-                print("{}: {}".format(i, arg))
-            i += 1
+        print("{:d} arguments:".format(n-1))
+        for i in range(1, n):
+            print("{:d}: {}".format(i, argv[i]))
