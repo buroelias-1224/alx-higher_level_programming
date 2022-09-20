b49fe419e686b3dccba8db03e59795f0a4b49398
buroelias-1224 <104757613+buroelias-1224@users.noreply.github.com>
2022-09-20 13:19:36 +0300

    Update 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""A class with restrictions.
+"""
+
+
 class LockedClass:
+    """The locked class"""
 
-    __slots__ = ['first_name']
-
-    def __init__(self, first_name=''):
-        self.first_name = first_name
+    __slots__ = "first_name"
